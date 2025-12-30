8243d73e9a795f6c005a9c01909c8b014342a4b0
nilanoyon371 <nilanoyon371@gmail.com>
2025-12-30 01:45:24 +0000

    Fix Vercel build errors

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ pip install -r requirements.txt
 
 # 2. Install Playwright browsers
 echo "Installing Playwright browsers..."
-playwright install --with-deps
+python -m playwright install --with-deps
